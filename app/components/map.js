988655e50c72b431f311f84b7f7597d8e89a2086
xiaoying wen <xiaoyingwen@yahoo.com>
2017-09-08 11:41:50 -0400

    not used, should remove this

--- app/components/map.js
+++ app/components/map.js
@@ -6,28 +6,17 @@ class Map extends React.Component {
    
   constructor(props) {
     super(props);
-    this.initMap = this.initMap.bind(this);
-}
+  }
 
 
-render() {
-  return (
-    <div>
-      <h1> Map</h1>
-      <div id="map"></div>
-   </div>
-  );
-}
-initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
-    
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: this.props.map
-    });
+  render() {
+    return (
+      <div>
+        <h1> Map</h1>
+        <div id="map"></div>
+     </div>
+    );
   }
 }
-
-
 
 export default Map;
