b34dbf51dad28ce6a78d66c73598c88810ad7e3e
xiaoying wen <xiaoyingwen@yahoo.com>
2017-09-12 21:52:30 -0400

    update for save

--- app/components/Save.js
+++ app/components/Save.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import PacApiHelper from "./PacApiHelper";
 const helpers = require("./utils/helpers");
-
 
 class Save extends React.Component {
    
   constructor(props) {
     super(props);
-    this.savePac = this.savePac.bind(this);
+    this.state = {
+        pacId: null,
+    };
+    // this.savePac = this.savePac.bind(this);
+    this.handleSavePac = this.handleSavePac.bind(this);
   }
 
   componentDidUpdate() {
     console.log(this.props.places);
   }
 
-  savePac(){
+/*  savePac(){
     var title = this.props.title;
     console.log('title here: ' + title);
     var addedPlaces = this.props.places;
@@ -32,12 +36,67 @@ class Save extends React.Component {
 
       helpers.savePac(placeName, address, placeId, lat, long);
     })
-  }
+  }*/
 
+  handleSavePac(){
+    var pac = {
+            title: this.props.title,
+            pictureURL: "",
+            category: "place",
+            createdBy: "59b7a8aae752690e4841dc73"
+    };
+    console.log('call handleSavePac' + JSON.stringify(this.props.places));
+
+    var addedPlaces = [];
+    this.props.places.map((place, index) => {
+      var pacplace={
+        "name": place.name,
+        "lat": place.geometry.location.lat(),
+        "lng": place.geometry.location.lng(),
+        "address": place.formatted_address,
+        "city": "",
+        "state": "",
+        "zipcode": "10065",
+        "country": "USA",
+        "officialWebsite": "http://www.centralparknyc.org/",
+        "googlePlaceId": "ChIJ4zGFAZpYwokRGUGph3Mf37k"};
+      addedPlaces.push(pacplace);
+    });
+
+    pac.places = addedPlaces;
+
+    console.log('call handleSavePac' + JSON.stringify(pac));
+    if(this.state.pacId != null) {
+      console.log('call update handleSavePac' + JSON.stringify(pac));
+      PacApiHelper.updatePac(pac)
+        .then(data => {
+         console.log("get updated pac is: -----------------" );
+                console.log(JSON.stringify(data));
+
+          console.log("updated pac is: -----------------" );
+          console.log(this.state);
+      });
+    }
+    else {
+      console.log('call save new handleSavePac' + JSON.stringify(pac));
+      PacApiHelper.addPac(pac)
+        .then(data => {   
+         console.log("get saved new pac is: -----------------" );
+                console.log(JSON.stringify(data));
+          
+          this.setState(prevState => ({
+            pacId: data._id
+          }));
+          console.log("saved pac is: -----------------" );
+          console.log(this.state);
+      });
+    }  
+  }
+  
   render() {
     return (
       <div>
-        <button onClick={this.savePac} className="save-btn" data-toggle="modal" data-target="#myModal"> Save Pack </button>
+        <button onClick={this.handleSavePac} className="save-btn" data-toggle="modal" data-target="#myModal"> Save Pack </button>
      </div>
     
     );
--- controllers/routes.js
+++ controllers/routes.js
@@ -92,7 +92,7 @@ module.exports = function(app) {
 
     app.get('/favorite-places', ensureAuthenticated, function(req, res, next) {
         //res.render('favorite');
-        Place2.find({}).exec(function(err, doc) {
+/*        Place2.find({}).exec(function(err, doc) {
 
             let pacData = {
                 pac: doc
@@ -106,7 +106,37 @@ module.exports = function(app) {
                     pacData
                 });
             }
-        });
+        });*/
+            var pacUser = getPacUserInfoFromGmailAcct(req.user._json);
+            User.findOne({
+                emailAddress: pacUser.emailAddress
+            }) // ..and populate all of the pacs for the user
+            .populate({
+                path: "pacs",
+                options: {
+                    sort: [{
+                        "createdAt": -1
+                    }]
+                }
+            }).exec(function(error, existinguser) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    if (!existinguser) { //cannot use existinguser.length===0 for findOne function
+                        var doc = [];
+                        let pacData = {
+                        pac: doc
+                        }
+                        res.render('my-packs', { pacData});
+                    } else {
+                        console.log(JSON.stringify(existinguser));
+                        let pacData = {
+                            pac: existinguser.pacs[0].places
+                        }
+                        res.render('my-packs', { pacData });
+                    }
+                }
+            }); //end lookup storyId and save record if it is a new story
     });
 
     app.get('/my-pacs', ensureAuthenticated, function(req, res, next) {
