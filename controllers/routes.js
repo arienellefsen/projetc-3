4373f3fcaedfc05485ef7fae5bb519ab0ca0b32d
Kalpana Gundrathi <kalpanasan@gmail.com>
2017-09-13 17:34:10 -0400

    updated to use correct handlerbars file for favorite places

--- controllers/routes.js
+++ controllers/routes.js
@@ -129,7 +129,7 @@ module.exports = function(app) {
     //   request.  If authentication fails, the user will be redirected back to the
     //   login page.  Otherwise, the primary route function function will be called,
     //   which, in this example, will redirect the user to the home page.
-    app.get('/auth/google/callback',
+    app.get('https://pacnshare.herokuapp.com/auth/google/callback',
         passport.authenticate('google', {
             successRedirect: '/dashboard',
             failureRedirect: '/'
